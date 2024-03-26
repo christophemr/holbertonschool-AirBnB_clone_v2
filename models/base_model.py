c76f8a53581ba3e91086d88b2f8d764ac0d5e9a0
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 22:00:27 +0100

    try a snipet

--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,50 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime, String
+from sqlalchemy import Column, String, DateTime
 import models
-from os import getenv
 
-
-time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
-
-if getenv("HBNB_TYPE_STORAGE") == 'db':
-    Base = declarative_base()
-else:
-    Base = object
+Base = declarative_base()
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """Defines all common attributes/methods for other models."""
+    if models.storage_t == 'db':
+        id = Column(String(60), primary_key=True, nullable=False)
+        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        id = Column(String(60), nullable=False, primary_key=True)
-        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-    def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            setattr(self, key, value)
-            if type(self.created_at) is str:
-                self.created_at = datetime.strptime(self.created_at, time_fmt)
-            if type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(self.updated_at, time_fmt)
-
-    def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+    def __init__(self, **kwargs):
+        """Initializes the base model."""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.utcnow()
+        else:
+            kwargs.setdefault('id', str(uuid.uuid4()))
+            kwargs.setdefault('created_at', datetime.utcnow())
+            kwargs.setdefault('updated_at', datetime.utcnow())
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at'] and isinstance(value, str):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.now()
+        """Updates 'updated_at' with the current datetime and saves the model."""
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
+    def delete(self):
+        """Deletes the current instance from the storage."""
+        models.storage.delete(self)
+
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        for key, value in self.__dict__.items():
-            if key == 'created_at' or key == 'updated_at':
-                dictionary[key] = value.isoformat()
-            elif key != '_sa_instance_state':
-                dictionary[key] = value
+        """Returns a dictionary containing all keys/values of the instance."""
+        dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
+        if 'created_at' in dictionary:
+            dictionary['created_at'] = self.created_at.isoformat()
+        if 'updated_at' in dictionary:
+            dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
