d3eb93efda502d10320e0f679e681af49ee9d2e2
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 16:49:18 +0100

    Dynamically select storage backend in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object
-    of class FileStorage
-    if the environmental variable is set to "db",
-    instantiates a dbstorage
-    otherwise, instantiates FileStorage
-"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
 from os import getenv
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
