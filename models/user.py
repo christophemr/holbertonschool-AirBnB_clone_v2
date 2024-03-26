f95c38c28a0701e86a34507ad2b127b0f03c069c
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 18:14:05 +0100

    Enhanced User model by inheriting from BaseModel and Base, and added database columns for email, password, first_name, and last_name as per specifications

--- models/user.py
+++ models/user.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+
+Base = declarative_base()
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
