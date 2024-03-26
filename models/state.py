8b5b3171d09b1d704728f73d3d5382951e919b93
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 18:28:51 +0100

    Enhance State model with table definition and relationship to City

--- models/state.py
+++ models/state.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    __tablename__ = "states"
+class State(BaseModel, Base):
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',
                           cascade='all, delete-orphan')
