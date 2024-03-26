17ae96ac901796925cd5b19e9b24357d9f8a208f
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 18:44:24 +0100

    Enhance BaseModel for improved attribute handling and storage compatibility

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,11 +7,13 @@ from sqlalchemy import Column, DateTime, String
 import models
 from os import getenv
 
+# Conditionally create the Base class for SQLAlchemy or a dummy object
 Base = declarative_base() if getenv("HBNB_TYPE_STORAGE") == 'db' else object
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    # Define columns if in DB storage mode
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         id = Column(String(60), primary_key=True, nullable=False)
         created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
@@ -19,22 +21,23 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs or "id" not in kwargs:
+        if 'id' not in kwargs:
             self.id = str(uuid.uuid4())
-        if not kwargs or "created_at" not in kwargs:
+        if 'created_at' not in kwargs:
             self.created_at = datetime.utcnow()
-        if not kwargs or "updated_at" not in kwargs:
-            self.updated_at = self.created_at
+        if 'updated_at' not in kwargs:
+            self.updated_at = datetime.utcnow()
+
         for key, value in kwargs.items():
             if key not in ("__class__", "_sa_instance_state"):
-                if key in ("created_at", "updated_at"):
+                if key in ("created_at", "updated_at") and isinstance(value, str):
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls_name = type(self).__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls_name, self.id, self.to_dict())
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -46,8 +49,10 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = type(self).__name__
-        dictionary['created_at'] = dictionary['created_at'].isoformat()
-        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        dictionary['created_at'] = self.created_at.isoformat() if isinstance(
+            self.created_at, datetime) else self.created_at
+        dictionary['updated_at'] = self.updated_at.isoformat() if isinstance(
+            self.updated_at, datetime) else self.updated_at
         dictionary.pop("_sa_instance_state", None)
         return dictionary
 
