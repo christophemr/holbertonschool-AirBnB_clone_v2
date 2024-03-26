c2e4728951d2ae86ddf2708aef6648a3a90a842d
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 16:09:59 +0100

    Revert "create a new sestion + a new database session"
    This reverts commit 0e17089eede3d2e9cd81361bd9c65a7640bc6235.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -36,12 +36,6 @@ class DBStorage:
         if env == 'test':
             Base.metadata.drop_all(self.__engine)
 
-        # Create a new session
-        self.__session = sessionmaker(bind=self.__engine,
-                                      expire_on_commit=False)
-        Session = scoped_session(self.__session)
-        self.__session = Session()
-
     def all(self, cls=None):
         """Query on the current database session"""
         objects = {}
@@ -75,3 +69,8 @@ class DBStorage:
         """Create all tables in the database"""
         # Create tables based on metadata
         Base.metadata.create_all(self.__engine)
+        # Create a new database session
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
