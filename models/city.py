23beff3a1279114fcb761c0f4efd706f78ef2ed7
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 23:24:59 +0100

    dell the from sqlalchemy.orm import relationship

--- models/city.py
+++ models/city.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
 
 class City(BaseModel, Base):
     """The City class"""
