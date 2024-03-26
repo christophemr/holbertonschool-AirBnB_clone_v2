68cf4b10627de30dbe4f33d5e13122d5c635afe1
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 16:48:22 +0100

    Enhance State model to handle relationships in both storage types

--- models/state.py
+++ models/state.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    __tablename__ = "states"
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
+
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """FileStorage relationship between State and City"""
+            from models import storage
+            from models.city import City
+            all_cities = storage.all(City)
+            return [city for city in all_cities.values() if city.state_id == self.id]
