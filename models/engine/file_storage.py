3e36a8c2f342872232aec0cddf81829b00c56233
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 17:09:40 +0100

    try some changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,50 +11,39 @@ from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """
-        Returns a dictionary of models currently in storage.
-        If cls is provided, returns a dictionary of objects of type cls.
-        """
-        if cls is not None:
-            return {key: obj for key, obj in FileStorage.__objects.items()
-                    if isinstance(obj, cls)}
-        return FileStorage.__objects
+        if cls:
+            cls_name = cls if isinstance(cls, str) else cls.__name__
+            return {k: v for k, v in self.__objects.items() if cls_name in k}
+        return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        temp = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        obj_dict = {obj_id: obj.to_dict()
+                    for obj_id, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
-            json.dump(temp, f)
+            json.dump(obj_dict, f)
 
     def delete(self, obj=None):
-        """
-        Deletes obj from __objects if it’s inside.
-        If obj is equal to None, the method should not do anything.
-        """
-        if obj is None:
-            return
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
 
     def reload(self):
-        """Loads storage dictionary from file"""
         try:
             with open(self.__file_path, 'r') as f:
-                objects = json.load(f)
-            for obj_id, obj_dict in objects.items():
-                cls_name = obj_dict['__class__']
+                obj_dict = json.load(f)
+            for obj_id, obj_data in obj_dict.items():
+                cls_name = obj_data['__class__']
                 if cls_name in globals():
-                    self.__objects[obj_id] = globals()[cls_name](**obj_dict)
+                    self.__objects[obj_id] = globals()[cls_name](**obj_data)
         except FileNotFoundError:
             pass
