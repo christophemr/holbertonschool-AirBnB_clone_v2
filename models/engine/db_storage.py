2f201097f747174426c3cc1d66bee3a744936cfd
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-26 17:25:09 +0100

    undo the changes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,6 @@
 """DB Storage Engine Module"""
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.user import User
@@ -22,74 +21,58 @@ class DBStorage:
 
     def __init__(self):
         """Initialize DBStorage"""
-        try:
-            # Get MySQL configuration from environment variables
-            user = getenv("HBNB_MYSQL_USER")
-            pwd = getenv("HBNB_MYSQL_PWD")
-            host = getenv("HBNB_MYSQL_HOST")
-            db = getenv("HBNB_MYSQL_DB")
-            env = getenv("HBNB_ENV")
+        # Get MySQL configuration from environment variables
+        user = getenv("HBNB_MYSQL_USER")
+        pwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
 
-            # Create the database engine
-            self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, pwd, host, db),
-                                          pool_pre_ping=True)
-            # Drop all tables if environment is 'test'
-            if env == 'test':
-                Base.metadata.drop_all(self.__engine)
+        # Create the database engine
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(user, pwd, host, db),
+                                      pool_pre_ping=True)
+        # Drop all tables if environment is 'test'
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
 
-            self.reload()
-        except SQLAlchemyError as e:
-            print("Error connecting to the database: {}".format(e))
+        self.reload()
 
     def all(self, cls=None):
         """Query on the current database session"""
         objects = {}
-        try:
-            if cls:
+        # Query objects based on the class
+        if cls:
+            query = self.__session.query(cls).all()
+        else:
+            # Query all types of objects if cls is not specified
+            for cls in self.all_classes.values():
                 query = self.__session.query(cls).all()
-            else:
-                # Query all types of objects if cls is not specified
-                for cls in self.all_classes.values():
-                    query = self.__session.query(cls).all()
-                    for obj in query:
-                        key = "{}.{}".format(type(obj).__name__, obj.id)
-                        objects[key] = obj
-            return objects
-        except SQLAlchemyError as e:
-            print("Error querying the database: {}".format(e))
-            return {}
+                # Format objects as dictionary
+                for obj in query:
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    objects[key] = obj
+        return objects
 
     def new(self, obj):
         """Add the object to the current database session"""
-        try:
-            self.__session.add(obj)
-        except SQLAlchemyError as e:
-            print("Error adding object to session: {}".format(e))
+        self.__session.add(obj)
 
     def save(self):
         """Commit all changes of the current database session"""
-        try:
-            self.__session.commit()
-        except SQLAlchemyError as e:
-            print("Error committing changes: {}".format(e))
+        self.__session.commit()
 
     def delete(self, obj=None):
         """Delete from the current database session"""
         if obj:
-            try:
-                self.__session.delete(obj)
-            except SQLAlchemyError as e:
-                print("Error deleting object: {}".format(e))
+            self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and initialize a new session"""
-        try:
-            # Create tables based on metadata
-            Base.metadata.create_all(self.__engine)
-            # Create a new database session
-            session_factory = sessionmaker(
-                bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(session_factory)
-            self.__session = Session()
-        except SQLAlchemyError as e:
-            print("Error reloading the database: {}".format(e))
+        """Create all tables in the database"""
+        # Create tables based on metadata
+        Base.metadata.create_all(self.__engine)
+        # Create a new database session
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
