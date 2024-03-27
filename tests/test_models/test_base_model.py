bfaeb1b924a195047603e0348137c15ca43e2468
christophemr <christophemr59@gmail.com>
2024-03-27 12:13:10 +0100

    test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,46 +1,56 @@
 #!/usr/bin/python3
 """ """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import unittest
-import datetime
+from datetime import datetime
 from uuid import UUID
 import json
 import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """ base_model test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """init basemodel test class """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """set up method """
         pass
 
     def tearDown(self):
+        """teardown method"""
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
+    def test_init(self):
+        """Tests the initialization of the model class.
+        """
+        self.assertIsInstance(self.value(), BaseModel)
+        if self.value is not BaseModel:
+            self.assertIsInstance(self.value(), Base)
+        else:
+            self.assertNotIsInstance(self.value(), Base)
+
     def test_default(self):
-        """ """
+        """default test """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """bm test with kwargs """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """bm test with int kwargs"""
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -57,41 +67,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """str method test """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """test to_dict method  """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """ kwargs test with none"""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """kwargs test with one """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ """
+        """id test of model """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """created at test """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """updated at test """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,13 +2,14 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import os
 
 
 class test_state(test_basemodel):
-    """ """
+    """test state class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ test state class init"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
@@ -16,4 +17,6 @@ class test_state(test_basemodel):
     def test_name3(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
