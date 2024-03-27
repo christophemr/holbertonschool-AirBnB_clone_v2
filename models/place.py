d57fbe76fe905afcdcbde75a674e39360cf7e226
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-28 00:10:25 +0100

    refactor place.py

--- models/place.py
+++ models/place.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
 from models.amenity import Amenity
-from sqlalchemy import Table
 from os import getenv
 import models
 
+# Association table for the many-to-many relationship between Place and Amenity
 place_amenity = Table(
     'place_amenity', Base.metadata,
     Column('place_id', String(60), ForeignKey(
@@ -17,10 +17,11 @@ place_amenity = Table(
         'amenities.id'), primary_key=True, nullable=False)
 )
 
+
 class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
+    """A place to stay"""
     id = Column(String(60), primary_key=True)
+    __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -31,29 +32,26 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship("Review", backref="place",
-                           cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
     amenity_ids = []
 
-    @property
-    def reviews(self):
-        """Getter for file storage to simulate DB relationship."""
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
+    # Relationships
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship(
+        "Amenity", secondary=place_amenity, viewonly=False)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews_fs(self):
+            """File storage relationship simulation for reviews."""
             return [review for review in models.storage.all(Review).values() if review.place_id == self.id]
-        return self._reviews
 
         @property
-        def amenities(self):
-            """Get/set linked Amenities."""
-            amenity_list = []
-            for amenity in list(models.storage.all(Amenity).values()):
-                if amenity.id in self.amenity_ids:
-                    amenity_list.append(amenity)
-            return amenity_list
+        def amenities_fs(self):
+            """File storage getter for amenities."""
+            return [amenity for amenity in models.storage.all(Amenity).values() if amenity.id in self.amenity_ids]
 
-        @amenities.setter
-        def amenities(self, value):
-            if isinstance(value, Amenity):
-                self.amenity_ids.append(value.id)
+        @amenities_fs.setter
+        def amenities_fs(self, obj):
+            """File storage setter for amenities."""
+            if type(obj).__name__ == 'Amenity':
+                self.amenity_ids.append(obj.id)
