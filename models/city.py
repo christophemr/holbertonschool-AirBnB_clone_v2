e2fb96270d1a034704a373899d675bfc2d963ed1
christophemr <christophemr59@gmail.com>
2024-03-27 09:36:33 +0100

    undo the changes

--- models/city.py
+++ models/city.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from os import getenv
 from sqlalchemy.orm import relationship
-import models
-import sqlalchemy
 
 
 class City(BaseModel, Base):
     """The City class"""
-    if models.data_storage_type == "db":
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
-        state_id = ""
-        name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
--- models/state.py
+++ models/state.py
@@ -3,27 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models
-from models.city import City
-from os import getenv
 
 
 class State(BaseModel, Base):
     """The State class"""
-    if models.data_storage_type == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        name = ""
-
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
