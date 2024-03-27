a0877fb9d19cc9bd69909495f76624a07b029c80
christophemr <christophemr59@gmail.com>
2024-03-27 09:27:40 +0100

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,13 +14,14 @@ from models.review import Review
 import models
 import sqlalchemy
 
+classes = {'User': User, 'State': State, 'City': City,
+           'Amenity': Amenity, 'Place': Place, 'Review': Review}
+
 
 class DBStorage:
     """DB Storage Engine Class"""
     __engine = None
     __session = None
-    all_classes = {'User': User, 'State': State, 'City': City,
-                   'Amenity': Amenity, 'Place': Place, 'Review': Review}
 
     def __init__(self):
         """Initialize DBStorage"""
@@ -45,52 +46,30 @@ class DBStorage:
     def all(self, cls=None):
         """Query on the current database session"""
         objects = {}
-        try:
-            if cls:
-                query = self.__session.query(cls).all()
-            else:
-                # Query all types of objects if cls is not specified
-                for cls in self.all_classes.values():
-                    query = self.__session.query(cls).all()
-                    for obj in query:
-                        key = "{}.{}".format(type(obj).__name__, obj.id)
-                        objects[key] = obj
-            return objects
-        except SQLAlchemyError as e:
-            print("Error querying the database: {}".format(e))
-            return {}
+        for clss in classes:
+            if cls is None or cls == classes[clss] or cls == clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    objects[key] = obj
+        return (objects)
 
     def new(self, obj):
         """Add the object to the current database session"""
-        try:
-            self.__session.add(obj)
-        except SQLAlchemyError as e:
-            print("Error adding object to session: {}".format(e))
+        self.__session.add(obj)
 
     def save(self):
         """Commit all changes of the current database session"""
-        try:
-            self.__session.commit()
-        except SQLAlchemyError as e:
-            print("Error committing changes: {}".format(e))
+        self.__session.commit()
 
     def delete(self, obj=None):
         """Delete from the current database session"""
-        if obj:
-            try:
-                self.__session.delete(obj)
-            except SQLAlchemyError as e:
-                print("Error deleting object: {}".format(e))
+        if obj is not None:
+            self.__session.delete(obj)
 
     def reload(self):
         """Create all tables in the database and initialize a new session"""
-        try:
-            # Create tables based on metadata
-            Base.metadata.create_all(self.__engine)
-            # Create a new database session
-            session_factory = sessionmaker(
-                bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(session_factory)
-            self.__session = Session()
-        except SQLAlchemyError as e:
-            print("Error reloading the database: {}".format(e))
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
