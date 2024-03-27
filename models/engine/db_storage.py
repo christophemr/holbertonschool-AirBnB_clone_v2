b424d015dca3087cb4f5a3dc4ff2a5eb51265757
christophemr <christophemr59@gmail.com>
2024-03-27 09:07:49 +0100

    refactor to meet requirements

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,13 +4,15 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import models
+import sqlalchemy
 
 
 class DBStorage:
@@ -22,24 +24,23 @@ class DBStorage:
 
     def __init__(self):
         """Initialize DBStorage"""
-        try:
-            # Get MySQL configuration from environment variables
-            user = getenv("HBNB_MYSQL_USER")
-            pwd = getenv("HBNB_MYSQL_PWD")
-            host = getenv("HBNB_MYSQL_HOST")
-            db = getenv("HBNB_MYSQL_DB")
-            env = getenv("HBNB_ENV")
+        # Get MySQL configuration from environment variables
+        HBNB_MYSQL_USER = getenv("HBNB_MYSQL_USER")
+        HBNB_MYSQL_PWD = getenv("HBNB_MYSQL_PWD")
+        HBNB_MYSQL_HOST = getenv("HBNB_MYSQL_HOST")
+        HBNB_MYSQL_DB = getenv("HBNB_MYSQL_DB")
+        HBNB_ENV = getenv("HBNB_ENV")
 
-            # Create the database engine
-            self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, pwd, host, db),
-                                          pool_pre_ping=True)
-            # Drop all tables if environment is 'test'
-            if env == 'test':
-                Base.metadata.drop_all(self.__engine)
-
-            self.reload()
-        except SQLAlchemyError as e:
-            print("Error connecting to the database: {}".format(e))
+        # Create the database engine
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(HBNB_MYSQL_USER,
+                                              HBNB_MYSQL_PWD,
+                                              HBNB_MYSQL_HOST,
+                                              HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
+        # Drop all tables if environment is 'test'
+        if HBNB_ENV == 'test':
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Query on the current database session"""
