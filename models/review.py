1f591404628ac85e6be921c8bb8a289f4ad0a060
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-27 23:21:28 +0100

    add id to ensures that your SQLAlchemy models are correctly mapped to the underlying database schema + del the place_id user_id and text empty

--- models/review.py
+++ models/review.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel, Base
+"""Review module for the HBNB project"""
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
-
+from models.base_model import Base, BaseModel
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
-    __tablename__ = "reviews"
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    __tablename__ = 'reviews'
+    id = Column(String(60), primary_key=True)
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
