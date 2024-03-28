7bb280c98dfb6e7384ce674cf69f00b3d8a87f6a
christophemr <christophemr59@gmail.com>
2024-03-28 14:36:44 +0100

    hotfix

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,12 @@
     instantiates a dbstorage
     otherwise, instantiates FileStorage
 """
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
-if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
+if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
