90f999cb16673371823c927f720e4eba9d840eb9
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-28 11:26:17 +0100

    test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,7 @@
---   Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance.
-
+-- Create the hbnb_dev_db if it doesn't exist
+-- create user hbnb_dev with pwd hbnb_dev_pwd if it doesn't exist
+-- grants all privilege on hbnb_dev_db and select priv on performance_schema to user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
