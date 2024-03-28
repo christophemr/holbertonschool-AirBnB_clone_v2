442bb1fe9c21bd0cf3392900ccbd5d9ff916094a
christophemr <christophemr59@gmail.com>
2024-03-28 09:23:03 +0100

    conditional statement for storage type and relationship added

--- models/city.py
+++ models/city.py
@@ -3,12 +3,17 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
+
+data_storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
     """The City class"""
     __tablename__ = 'cities'
     id = Column(String(60), primary_key=True)
-    name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete")
+    name = Column(String(128), nullable=False)
+    if data_storage_type == 'db':
+        state = relationship("State", back_populates="cities")
+    places = relationship("Place", backref="cities", cascade="delete")
