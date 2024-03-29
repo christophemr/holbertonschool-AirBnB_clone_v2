5dc7fd733349c563ed4d8dc1260dd8da8421a821
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-29 21:41:41 +0100

    add tsome test to setup teardown ans save the file in DBStorage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,8 +3,9 @@
 Unit tests for tha database storage.
 """
 import unittest
-import pycodestyle
 from models.engine.db_storage import DBStorage
+from models.amenity import Amenity
+from models import storage
 
 
 class test_db_storage(unittest.TestCase):
@@ -39,3 +40,25 @@ class test_db_storage(unittest.TestCase):
 
         module_class = len(DBStorage.__init__.__doc__)
         self.assertGreater(module_class, 0)
+
+    def setUp(self):
+        """Setup test environment."""
+        storage.delete_all(
+            Amenity)
+
+    def tearDown(self):
+        """Teardown test environment."""
+        storage.delete_all(Amenity)
+
+    def test_save(self):
+        """Test saving an Amenity record."""
+        obj = Amenity(name="Pool")
+        obj.save()
+        self.assertIsNotNone(obj.id)
+
+        fetched_obj = storage.get(Amenity, obj.id)
+        self.assertIsNotNone(fetched_obj)
+        self.assertEqual(fetched_obj.name, "Pool")
+
+    if __name__ == '__main__':
+        unittest.main()
