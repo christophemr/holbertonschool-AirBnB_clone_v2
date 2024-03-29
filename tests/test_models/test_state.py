ac592758e3f44e3ba2d44194cb13f48787fe00e1
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-29 22:57:03 +0100

    ensures the test checks for the name attribute to always be a string

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -9,8 +9,7 @@ class test_state(TestCase):
     def test_name3(self):
         """Test that the 'name' attribute of a State instance is correctly typed."""
         new = State()
-        expected_type = str if os.getenv(
-            'HBNB_TYPE_STORAGE') != 'db' else type(None)
         new.name = "Test State"
-        self.assertIsInstance(new.name, expected_type, f"The 'name' attribute is expected to be of type {
-                              expected_type}, but got {type(new.name)}")
+        # The expectation is now that new.name is always a string, regardless of storage type
+        self.assertIsInstance(
+            new.name, str, "The 'name' attribute is expected to be of type 'str'")
