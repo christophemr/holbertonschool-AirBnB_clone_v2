c429a2fbc38bfbaf4b58c8828fdbecb917f2eaf6
christophemr <christophemr59@gmail.com>
2024-04-09 09:14:39 +0200

    script that displays python followed by the value of a variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application:"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """returns a string(hello)"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns a string(HBNB)"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """returns a C followed by the value of a variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """returns Python followed by the value of a variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
