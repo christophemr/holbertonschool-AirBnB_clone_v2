5ac5a872d1a3131273627a162028fb8a81139e4e
christophemr <christophemr59@gmail.com>
2024-04-09 11:41:59 +0200

    typo

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def pythonText(text="is cool"):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """returns "nis a number only if n is an integer"""
+    """returns n is a number only if n is an integer"""
     return "{} is a number".format(n)
 
 
