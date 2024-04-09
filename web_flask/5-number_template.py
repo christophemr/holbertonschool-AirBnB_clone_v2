7591d5ee723833a52da298a64de2f51d75df7184
christophemr <christophemr59@gmail.com>
2024-04-09 10:03:10 +0200

    conversion type added to the parameters

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -24,13 +24,14 @@ def cText(text):
     return "C {}".format(text.replace("_", " "))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pythonText(text="is cool"):
     """returns Python followed by the value of a variable"""
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """returns "nis a number only if n is an integer"""
     if isinstance(n, int):
