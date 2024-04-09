b2b09ddce6ff4dc5be130826ca1fff6d6a42cebe
christophemr <christophemr59@gmail.com>
2024-04-09 23:18:09 +0200

    hotfix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def display_states():
-    """state_list html page to display States list"""
-    states = storage.all()
+    """display States list"""
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
