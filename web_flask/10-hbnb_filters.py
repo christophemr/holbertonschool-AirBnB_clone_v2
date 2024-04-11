9eedad988826b7dc6f2502828b711a9c40dee15e
christophemr <christophemr59@gmail.com>
2024-04-11 09:36:50 +0200

    fix

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -5,20 +5,24 @@ starts a Flask web application
 
 from flask import Flask, render_template
 from models import *
+from models.city import City
 from models import storage
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/web_flask/static')
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def filters():
     """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
+    states = sorted(storage.all("State").values(),
+                    key=lambda state: state.name)
+    amenities = sorted(storage.all("Amenity").values(),
+                       key=lambda amenity: amenity.name)
+    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
     return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+                           amenities=amenities, cities=cities)
 
 
-@app.teardown_appcontext
+@ app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
